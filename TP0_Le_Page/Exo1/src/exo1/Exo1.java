30a200aabf62909c0a52e435a4372f09bdd8557b
Le_Page_Clarisse <clarisse.lepage@epfedu.fr>
2024-09-17 16:41:56 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_Le_Page/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,28 @@
+/*
+ * TP0_Exercice1
+ * TDC
+ *17 Septembre 2024
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author clale
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+    
+}
