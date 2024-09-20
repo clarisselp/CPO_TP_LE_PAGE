8e9ca553465cca964b6d4f3cc0a841cdab129c21
Le_Page_Clarisse <clarisse.lepage@epfedu.fr>
2024-09-20 15:41:18 +0200

    Création d'un nouveau projet versionné

--- TP0_Le_Page/calculator/src/calculator/Calculator.java
+++ TP0_Le_Page/calculator/src/calculator/Calculator.java
@@ -18,26 +18,46 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Please enter the operator: 1) add");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
         System.out.println("2) substract");
         System.out.println("3) multiply");
         System.out.println("4) divide");
         System.out.println("5) modulo");
         
         int operateur;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\n Entrer le nombre :");
+        System.out.println("\n Entrer le chiffre de l'opérateur :");
         operateur=sc.nextInt();
         
-        int operateur1;
-        //Scanner sc = new Scanner(System.in);
-        System.out.println("\n Entrer le nombre :");
-        operateur1=sc.nextInt();
+        int operande1;
+        System.out.println("\n Entrer le nombre 1 :");
+        operande1=sc.nextInt();
         
-        int operateur2;
-        //Scanner sc = new Scanner(System.in);
-        System.out.println("\n Entrer le nombre :");
-        operateur2=sc.nextInt();
+        int operande2;
+        System.out.println("\n Entrer le nombre 2 :");
+        operande2=sc.nextInt();
+        
+        int result;
+        result=0;
+        
+        if (operateur==1){
+        result=operande1+operande2;
+        }
+        if (operateur==2){
+        result=operande1-operande2;
+        }
+        if (operateur==3){
+        result=operande1*operande2;
+        }
+        if (operateur==4){
+        result=operande1/operande2;
+        }
+        if (operateur==5){
+        result=operande1%operande2;
+        }
+        System.out.println("Le resultat est: "+result+"");
     }
     
 }
