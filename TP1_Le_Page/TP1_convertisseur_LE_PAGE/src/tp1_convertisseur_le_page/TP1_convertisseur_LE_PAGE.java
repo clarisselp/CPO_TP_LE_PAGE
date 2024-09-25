c1c9c2b40526d541151a463cc5d6f7285e113a7e
Le_Page_Clarisse <clarisse.lepage@epfedu.fr>
2024-09-25 16:40:26 +0200

    Création d'un nouveau projet versionné

--- TP1_Le_Page/TP1_convertisseur_LE_PAGE/src/tp1_convertisseur_le_page/TP1_convertisseur_LE_PAGE.java
+++ TP1_Le_Page/TP1_convertisseur_LE_PAGE/src/tp1_convertisseur_le_page/TP1_convertisseur_LE_PAGE.java
@@ -1,7 +1,7 @@
 /*
  * TP1_convertisseur
  * TDC
- * Réalisation un convertisseur de températures
+ * Réalisation d'un convertisseur de températures
  * 23 Septembre 2024
  */
 package tp1_convertisseur_le_page;
@@ -19,25 +19,58 @@ public class TP1_convertisseur_LE_PAGE {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //Initialisation de la première valeur
         double v1;
         //double deg_K;
         
+        //Saisi d'un nombre (temperature) par l'utilisateur
         System.out.println("\n Bonjour, saisissez une valeur :");
-        v1=sc.nextDouble(); // On demande a sc de donner le prochain entier
+        v1=sc.nextDouble();
+        //Saisi de la conversion que l'utilisateur souaite effectuer
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :");
+        System.out.println("1) De Celcius vers Kelvin");
+        System.out.println("2) De Kelvin vers Celcius");
+        System.out.println("3) De Farenheit vers Celcius");
+        System.out.println("4) De Celcius vers Farenheit");
+        System.out.println("5) De Kelvin vers Farenheit");
+        System.out.println("6) De Farenheit vers Kelvin");
         
-        System.out.println("Saisissez la conversion que vous souhaiter effectuer :")
-        
-        System.out.println("\n Bonjour, saisissez une valeur :");
-        v1=sc.nextDouble(); // On demande a sc de donner le prochain entier
         
         //deg_K=v1+273.15;
         //System.out.println("La température en Kelvin est de: "+deg_K+" K.");
-        System.out.println("La température en Kelvin est de: "+CelciusVersKelvin(v1)+" K.");
-        System.out.println("La température en Kelvin est de: "+KelvinVersCelcius(v1)+" K.");
-        System.out.println("La température en Kelvin est de: "+FarenheitVersCelcius(v1)+" K.");
-        System.out.println("La température en Kelvin est de: "+CelciusVersFarenheit(v1)+" K.");
-        System.out.println("La température en Kelvin est de: "+KelvinVersFarenheit(v1)+" K.");
-        System.out.println("La température en Kelvin est de: "+FarenheitVersKelvin(v1)+" K.");
+        //System.out.println("La température en Kelvin est de: "+CelciusVersKelvin(v1)+" K.");
+        //System.out.println("La température en Kelvin est de: "+KelvinVersCelcius(v1)+" K.");
+        //System.out.println("La température en Kelvin est de: "+FarenheitVersCelcius(v1)+" K.");
+        //System.out.println("La température en Kelvin est de: "+CelciusVersFarenheit(v1)+" K.");
+        //System.out.println("La température en Kelvin est de: "+KelvinVersFarenheit(v1)+" K.");
+        //System.out.println("La température en Kelvin est de: "+FarenheitVersKelvin(v1)+" K.");
+       
+        int conversion;
+        conversion = sc.nextInt();
+        if (conversion==1){
+            System.out.println(v1 + " degre Celsius est egal a " + CelciusVersKelvin(v1) + " degres Kelvin");
+        }
+       
+        if (conversion==2){
+            System.out.println(v1 + " degre Kelvin est egal a " + KelvinVersCelcius(v1) + " degres Celsius");
+        }
+       
+        if (conversion==3){
+            System.out.println(v1 + " degre Farenheit est egal a " + FarenheitVersCelcius(v1) + " degres Celsius");
+        }
+       
+        if (conversion==4){
+            System.out.println(v1 + " degre Celsius est egal a " + CelciusVersFarenheit(v1) + " degres Farenheit");
+        }
+       
+        if (conversion==5){
+            System.out.println(v1 + " degre Kelvin est egal a " + KelvinVersFarenheit(v1) + " degres Farenheit");
+        }
+       
+        if (conversion==6){
+            System.out.println(v1 + " degre Farenheit est egal a " + FarenheitVersKelvin(v1) + " degres Kelvin");
+        }
+       
     }
     public static double CelciusVersKelvin (double tCelcius) {
         double tKelvin;
