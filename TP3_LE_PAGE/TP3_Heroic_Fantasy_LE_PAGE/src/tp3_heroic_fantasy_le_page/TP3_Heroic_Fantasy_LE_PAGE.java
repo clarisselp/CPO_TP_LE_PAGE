6b797d2f871a58b687676d7d35896385ac612141
Le_Page_Clarisse <clarisse.lepage@epfedu.fr>
2024-10-23 15:18:23 +0200

    Création d'un nouveau projet versionné

---
+++ TP3_LE_PAGE/TP3_Heroic_Fantasy_LE_PAGE/src/tp3_heroic_fantasy_le_page/TP3_Heroic_Fantasy_LE_PAGE.java
@@ -0,0 +1,24 @@
+/*
+ * TP3_Heroic_Fantasy_LE_PAGE
+ * TDC
+ * création des armes de 2 types - l’épée et le bâton, que tous les personnages peuvent 
+utiliser avec plus ou moins d’efficacité – et des personnages de 2 types : un magicien et un guerrier. Chaque 
+classe de personnage a son arme de prédilection. Utilisatuin de l’héritage simple pour avoir un modèle le plus pertinent et extensible possible.
+ * 23 Octobre 2024
+ */
+package tp3_heroic_fantasy_le_page;
+
+/**
+ *
+ * @author clale
+ */
+public class TP3_Heroic_Fantasy_LE_PAGE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
