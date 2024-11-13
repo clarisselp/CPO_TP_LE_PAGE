4453e157b4f0ea673fee10314ab8962b3840fd94
Le_Page_Clarisse <clarisse.lepage@epfedu.fr>
2024-11-13 12:58:27 +0100

    Création d'un nouveau projet versionné

--- TP3_LE_PAGE/TP3_Heroic_Fantasy_LE_PAGE/src/tp3_heroic_fantasy_le_page/TP3_Heroic_Fantasy_LE_PAGE.java
+++ TP3_LE_PAGE/TP3_Heroic_Fantasy_LE_PAGE/src/tp3_heroic_fantasy_le_page/TP3_Heroic_Fantasy_LE_PAGE.java
@@ -8,6 +8,8 @@ classe de personnage a son arme de prédilection. Utilisatuin de l’héritage
  */
 package tp3_heroic_fantasy_le_page;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author clale
@@ -18,7 +20,36 @@ public class TP3_Heroic_Fantasy_LE_PAGE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Création des épées avec des paramètres différents
+        Epee excalibur = new Epee("Excalibur", 7, 5);
+        Epee durandal = new Epee("Durandal", 4, 7);
+
+        // Création des bâtons avec des paramètres différents
+        Baton chene = new Baton("Chene", 4, 5);
+        Baton charme = new Baton("Charme", 5, 6);
+
+        // Affichage des informations des objets créés
+        System.out.println(excalibur);
+        System.out.println(durandal);
+        System.out.println(chene);
+        System.out.println(charme);
+        
+        // Création d'un ArrayList pour stocker les armes
+        ArrayList<Arme> armes = new ArrayList<Arme>(); 
+    
+        // Ajout des armes dans le tableau dynamique
+        armes.add(excalibur);
+        armes.add(durandal);
+        armes.add(chene);
+        armes.add(charme);
+
+        // Affichage des caractéristiques de chaque arme en parcourant le Vector
+        for (int i = 0; i < armes.size(); i++) {
+            System.out.println(armes.get(i));
+        }
+        
+
     }
     
 }
