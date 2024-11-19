a6a8b57e7b919117113b0f16d1a9cc7e7e5da27e
Le_Page_Clarisse <clarisse.lepage@epfedu.fr>
2024-11-19 13:42:38 +0100

    Création d'un nouveau projet versionné

--- TP3_LE_PAGE/TP3_Heroic_Fantasy_LE_PAGE/src/Personnages/Personnage.java
+++ TP3_LE_PAGE/TP3_Heroic_Fantasy_LE_PAGE/src/Personnages/Personnage.java
@@ -4,6 +4,9 @@
  */
 package Personnages;
 
+import java.util.ArrayList;
+import Armes.Arme;
+
 /**
  *
  * @author clale
@@ -12,11 +15,13 @@ public abstract class Personnage {
     
     private String nom;
     private int niveauVie;
+    private ArrayList<Arme> armes;
 
     // Constructeur pour initialiser le nom et le niveau de vie
     public Personnage(String nom, int niveauVie) {
         this.nom = nom;
         this.niveauVie = niveauVie;
+        this.armes = new ArrayList<>();
     }
 
     // Getter pour le niveau de vie
@@ -27,6 +32,21 @@ public abstract class Personnage {
     // Getter pour le nom
     public String getNom() {
         return nom;
+    }
+    
+    // Méthode pour ajouter une arme
+    public void ajouterArme(Arme arme) {
+        if (armes.size() < 5) { // Vérifie si le personnage a moins de 5 armes
+            armes.add(arme);
+            System.out.println("L'arme " + arme.getNom() + " a été ajoutée à " + nom + ".");
+        } else {
+            System.out.println("Le personnage " + nom + " ne peut pas porter plus de 5 armes.");
+        }
+    }
+
+    // Getter pour accéder à la liste des armes
+    public ArrayList<Arme> getArmes() {
+        return armes;
     }
 
     // Redéfinition de la méthode toString pour afficher les informations du personnage
